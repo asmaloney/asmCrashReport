asmaloney/asmCrashReport

commit 8c3c6e19d6867496a52fbf2642df9a60651b564a

Rename _addr2line to _addressToLine
On macOS we use "atos" and on Windows we use "addr2line", so use a more generic name for the method.

src/asmCrashReport.cpp
@@ -93,7 +93,7 @@ namespace asmCrashReport
    }
 
    // Resolve symbol name & source location
-   QString _addr2line( const QString &inProgramName, void const * const inAddr )
+   QString _addressToLine( const QString &inProgramName, void const * const inAddr )
    {
       const QString  cAddrStr = QStringLiteral( "0x%1" ).arg( quintptr( inAddr ), 16, 16, QChar( '0' ) );
 
@@ -250,9 +250,9 @@ namespace asmCrashReport
       {
           // https://stackoverflow.com/a/38019482
 #ifdef _M_IX86
-         frameInfoList += _addr2line( sProgramName, reinterpret_cast<void*>(inExceptionInfo->ContextRecord->Eip) );
+         frameInfoList += _addressToLine( sProgramName, reinterpret_cast<void*>(inExceptionInfo->ContextRecord->Eip) );
 #elif _M_X64
-         frameInfoList += _addr2line( sProgramName, reinterpret_cast<void*>(inExceptionInfo->ContextRecord->Rip) );
+         frameInfoList += _addressToLine( sProgramName, reinterpret_cast<void*>(inExceptionInfo->ContextRecord->Rip) );
 #else
 #error You need to implement the call to _addr2line for this architecture
 #endif
@@ -293,7 +293,7 @@ namespace asmCrashReport
 
          if ( !cSymbol.isNull() )
          {
-            QString  locationStr = _addr2line( sProgramName, sStackTraces [i] );
+            QString  locationStr = _addressToLine( sProgramName, sStackTraces[i] );
 
             if ( !locationStr.isEmpty() )
             {

commit d202cfef110c0d0daa07f440de927e1c02b72672

Fix Windows build (#9)
Commit 8c3c6e19d6867496a52fbf2642df9a60651b564a broke the Windows build...

src/asmCrashReport.cpp
@@ -181,7 +181,7 @@ namespace asmCrashReport
                  SymFunctionTableAccess64, SymGetModuleBase64, nullptr )
               )
       {
-         QString  locationStr = _addr2line( sProgramName, reinterpret_cast<void*>(stackFrame.AddrPC.Offset) );
+         QString  locationStr = _addressToLine( sProgramName, reinterpret_cast<void*>(stackFrame.AddrPC.Offset) );
 
          // match the mangled name and demangle if we can
          QRegularExpressionMatch match = sSymbolMatching.match( locationStr );
@@ -275,7 +275,7 @@ namespace asmCrashReport
 #elif _M_X64
          frameInfoList += _addressToLine( sProgramName, reinterpret_cast<void*>(inExceptionInfo->ContextRecord->Rip) );
 #else
-#error You need to implement the call to _addr2line for this architecture
+#error You need to implement the call to _addressToLine for this architecture
 #endif
       }
       else